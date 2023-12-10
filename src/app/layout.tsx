ca580803f798b37d86031badb095d8c9dade5f8d
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-10 11:42:20 -0700

    Format frenzy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Chat from '@/components/Chat'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Chat from "@/components/Chat";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'OpenAi Chatbot',
-  description: 'Chatbot using OpenAi API',
-}
+    title: "OpenAi Chatbot",
+    description: "Chatbot using OpenAi API",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Chat />
-        {children}
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Chat />
+                {children}
+            </body>
+        </html>
+    );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,8 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 // allows us to write conditional classes
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-  // left-0 right-0 will be converted to inset-x-0
+    return twMerge(clsx(inputs));
+    // left-0 right-0 will be converted to inset-x-0
 }
