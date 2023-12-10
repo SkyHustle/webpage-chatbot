ff8194083aa39c009caadf82ee1f971685cb80bf
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-09 18:40:09 -0700

    Install and configure shadcn

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+// allows us to write conditional classes
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+  // left-0 right-0 will be converted to inset-x-0
+}
