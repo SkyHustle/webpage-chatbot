00a708b2689d0d5eb96c08b512da56523356fd08
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-10 11:55:58 -0700

    Chat header

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -20,7 +20,17 @@ const Chat: FC = () => {
                 <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden">
                     <div className="w-full h-full flex flex-col">
                         <AccordionTrigger className="px-6 border-b border-zinc-300">
-                            Chat Header
+                            <div className="w-full flex gap-3 justify-start items-center text-zinc-800">
+                                <div className="flex flex-col items-start text-sm">
+                                    <p className="text-xs">Chat with</p>
+                                    <div className="flex gap-1.5 items-center">
+                                        <p className="w-2 h-2 rounded-full bg-green-500" />
+                                        <p className="font-medium">
+                                            Support Bot
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </AccordionTrigger>
                         <AccordionContent>
                             <div className="flex flex-col h-80">
