7bfffaaa83b322ff8fcf83721b73a69cea95ea42
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-10 14:25:16 -0700

    Control input field

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,10 +1,12 @@
 import { cn } from "@/lib/utils";
-import { FC, HTMLAttributes } from "react";
+import { FC, HTMLAttributes, useState } from "react";
 import TextAreaAutosize from "react-textarea-autosize";
 
 interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
 
 const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
+    const [input, setInput] = useState<string>("");
+
     return (
         <div {...props} className={cn("border-t border-zinc-300", className)}>
             <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
@@ -13,6 +15,8 @@ const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
                     maxRows={4}
                     autoFocus
                     placeholder="Type a message"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
                     className="peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6"
                 />
             </div>
