0e5fa1b9d7128fd03365a55023130f4bde932029
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-10 13:25:25 -0700

    Add Basic resizeable textarea

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,12 +1,21 @@
 import { cn } from "@/lib/utils";
 import { FC, HTMLAttributes } from "react";
+import TextAreaAutosize from "react-textarea-autosize";
 
 interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
 
 const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
     return (
         <div {...props} className={cn("border-t border-zinc-300", className)}>
-            ChatInput
+            <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
+                <TextAreaAutosize
+                    minRows={1}
+                    maxRows={4}
+                    autoFocus
+                    placeholder="Type a message"
+                    className="peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6"
+                />
+            </div>
         </div>
     );
 };
