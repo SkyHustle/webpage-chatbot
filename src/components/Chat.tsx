046b092709e5cd8d8b60ceee9c32f952a49dd930
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-10 11:49:38 -0700

    Style and reposition chatbot

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,35 +1,34 @@
 "use client";
 
 import { FC } from "react";
+
 import {
     Accordion,
     AccordionContent,
     AccordionItem,
     AccordionTrigger,
-} from "@/components/ui/accordion";
+} from "./ui/accordion";
 
 const Chat: FC = () => {
     return (
-        <Accordion type="single" collapsible className="w-full">
+        <Accordion
+            type="single"
+            collapsible
+            className="relative bg-white z-40 shadow"
+        >
             <AccordionItem value="item-1">
-                <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                <AccordionContent>
-                    Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-2">
-                <AccordionTrigger>Is it styled?</AccordionTrigger>
-                <AccordionContent>
-                    Yes. It comes with default styles that matches the other
-                    components&apos; aesthetic.
-                </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-                <AccordionTrigger>Is it animated?</AccordionTrigger>
-                <AccordionContent>
-                    Yes. It&apos;s animated by default, but you can disable it
-                    if you prefer.
-                </AccordionContent>
+                <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden">
+                    <div className="w-full h-full flex flex-col">
+                        <AccordionTrigger className="px-6 border-b border-zinc-300">
+                            Chat Header
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            <div className="flex flex-col h-80">
+                                Chat Messages <br /> Chat Input
+                            </div>
+                        </AccordionContent>
+                    </div>
+                </div>
             </AccordionItem>
         </Accordion>
     );
