d375eb3c925fa86e68ffd0422734202078e7c8d3
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-10 14:58:30 -0700

    install and setup the foundation for react query

---
+++ src/components/Providers.tsx
@@ -0,0 +1,20 @@
+// client component that encapsulates all the Context Providers in our app
+"use client";
+
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { FC } from "react";
+
+interface ProvidersProps {
+    children: React.ReactNode;
+}
+
+const Providers: FC<ProvidersProps> = ({ children }) => {
+    const queryClient = new QueryClient();
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+};
+
+export default Providers;
