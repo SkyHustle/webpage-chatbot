3f9abd8d4458ad912a50394adee08e1bb8f207bb
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-11 11:17:38 -0700

    Define api endpoint, for security make sure the messages being sent are of the correct type

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,7 @@
+import { MessageArraySchema } from "@/lib/validators/message";
+
+export async function POST(req: Request) {
+    const { messages } = await req.json();
+
+    const parsedMessages = MessageArraySchema.parse(messages);
+}
