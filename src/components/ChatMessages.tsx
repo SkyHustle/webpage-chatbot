adbeed9389209ce63b8eb484798fc4b87882c5d4
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-15 09:46:08 -0700

    Display clickable links to books inside ai replies

--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -1,6 +1,7 @@
 import { MessagesContext } from "@/context/messages";
 import { cn } from "@/lib/utils";
 import { FC, HTMLAttributes, useContext } from "react";
+import MarkdownLite from "./MarkdownLite";
 
 // allows us to pass any prop that a normal div would take
 interface ChatMessagesProps extends HTMLAttributes<HTMLDivElement> {}
@@ -32,14 +33,22 @@ const ChatMessages: FC<ChatMessagesProps> = ({ className, ...props }) => {
                             className={cn(
                                 "flex flex-col space-y-2 text-sm max-w-xs mx-2 overflow-x-hidden",
                                 {
-                                    "bg-blue-600 text-white":
-                                        message.isUserMessage,
-                                    "bg-gray-200 text-gray-900":
+                                    "order-1 items-end": message.isUserMessage,
+                                    "order-2 items-start":
                                         !message.isUserMessage,
                                 }
                             )}
                         >
-                            {message.text}
+                            <p
+                                className={cn("px-4 py-2 rounded-lg", {
+                                    "bg-blue-600 text-white":
+                                        message.isUserMessage,
+                                    "bg-gray-200 text-gray-900":
+                                        !message.isUserMessage,
+                                })}
+                            >
+                                <MarkdownLite text={message.text} />
+                            </p>
                         </div>
                     </div>
                 </div>
