fd58c41699143fb271d28a5a999bfd2b7bebc29a
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-16 19:07:51 -0700

    Add pulsing green dot to indicate chatbot is online

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -23,11 +23,15 @@ const Chat = () => {
                             <div className="w-full flex gap-3 justify-start items-center text-zinc-800">
                                 <div className="flex flex-col items-start text-sm">
                                     <p className="text-xs">Chat with</p>
-                                    <div className="flex gap-1.5 items-center">
-                                        <p className="w-2 h-2 rounded-full bg-green-500" />
-                                        <p className="font-medium">
+
+                                    <div className="relative inline-flex">
+                                        <p className="inline-flex items-center px-4 py-2 text-sm leading-6  transition duration-150 ease-in-out">
                                             Support Bot
                                         </p>
+                                        <span className="absolute left-0 top-5 -mr-1 -mt-1 flex h-2 w-2">
+                                            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+                                            <span className="relative inline-flex h-2 w-2 rounded-full bg-green-500"></span>
+                                        </span>
                                     </div>
                                 </div>
                             </div>
